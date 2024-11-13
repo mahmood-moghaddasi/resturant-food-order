ffd1f64919e8e093c895f09caf699ce1b42ce5ff
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-11-13 13:27:17 +0330

    Home page created (Banner , Attributes , Definition)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
+import HomePage from "../components/templates/HomePage";
+
 export default function Home() {
-  return (
-    <div>
-      <h2>botofood</h2>
-    </div>
-  );
+  return <HomePage />;
 }
