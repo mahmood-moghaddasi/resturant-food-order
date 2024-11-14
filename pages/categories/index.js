80cfbbe34f33152a7f9954e83e38ff4533264696
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-11-14 17:54:00 +0330

    categories page created

---
+++ pages/categories/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import CategoriesPage from "../../components/templates/CategoriesPage";
+
+function categories() {
+  return <CategoriesPage />;
+}
+
+export default categories;
